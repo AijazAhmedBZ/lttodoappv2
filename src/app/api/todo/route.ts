0aa9893b15084940021819a8341532f9013967f8
aijazahmedbz <aijaz.ahmed.bz@gmail.com>
2023-07-31 18:18:37 +0500

    updated on 31-Jul-23

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -3,33 +3,21 @@ import { Todo, NewTodo, db, todoTable } from "@/lib/drizzle";
 import { sql } from "@vercel/postgres";
 import { eq } from "drizzle-orm";
 
-export async function GET(request: NextRequest) {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS Todos(id serial, task varchar(255), isDone boolean);`;
-    const res = await db.select().from(todoTable).execute();
-    return NextResponse.json({ data: res });
-    // console.log(res)
-  } catch (err) {
-    console.log((err as { message: "string" }).message);
-    return NextResponse.json({ message: "Something went wtong" });
-  }
-}
 
 export async function POST(request: NextRequest) {
   const req = await request.json();
   try {
     if (req.task) {
-      const res = await db
+      const res:NewTodo[] = await db
         .insert(todoTable)
         .values({
           task: req.task,
-          completed: false,
         })
         .returning();
       // console.log(res)
       return NextResponse.json({
-        message: "Data added successfully",
         data: res,
+        message: "Data added successfully",
       });
     } else throw new Error("Task field is required");
   } catch (error) {
@@ -39,30 +27,41 @@ export async function POST(request: NextRequest) {
   }
 }
 
-// export async function DELETE(
-//   request: NextRequest,
-//   // { params }: { params: { id: Todo } }
-// ) {
-//   const req = await request.json();
-//   // const id = params.id;
-//   try {
-//     if (req.task) {
-//       const res = await db
-//         .delete(todoTable)
-//         // .where(eq(todoTable.id, todoTable.completed))
-//         .returning({ id: todoTable.id, task: todoTable.task });
-//       console.log("result", res);
-//       return NextResponse.json({
-//         // message: "Data deleted successfully. updated ID: " + id,
-//         data: res,
-//       });
-//     } else throw new Error("Task field is required");
-//   } catch (error) {
-//     return NextResponse.json({
-//       message: (error as { message: "string" }).message,
-//     });
-//   }
-// }
+
+export const GET = async (request: NextRequest) => {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS Todos(id serial, task varchar(255), completed boolean);`;
+    const res:Todo[] = await db.select().from(todoTable).execute();
+    return NextResponse.json({ data: res });
+    // console.log(res)
+  } catch (err) {
+    console.log((err as { message: string }).message);
+    return NextResponse.json({ message: "Something went wrong" });
+  }
+}
+
+export const DELETE = async(request: NextRequest,{ params }: { params: { id: Todo } }
+) {
+  const req = await request.json();
+  const taskId = params.id;
+  try {
+    if (req.task) {
+      const res = await db
+        .delete(todoTable)
+        .where(eq(todoTable.id, taskId))
+        .returning({ id: todoTable.id, task: todoTable.task });
+      console.log("result", res);
+      return NextResponse.json({
+        // message: "Data deleted successfully. updated ID: " + id,
+        data: res,
+      });
+    } else throw new Error("Task field is required");
+  } catch (error) {
+    return NextResponse.json({
+      message: (error as { message: "string" }).message,
+    });
+  }
+}
 
 // export async function PUT(
 //   request: NextRequest,
